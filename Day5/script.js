7747a9e883bfd6c90a5acaeca5ca4fa5867bd439
Saurabhverma074 <saurabhverma1051@gmail.com>
2024-10-15 15:41:32 +0530

    js dom

---
+++ Day5/script.js
@@ -0,0 +1,17 @@
+// let x= confirm("are you sure")
+// alert(x)
+
+// let v=prompt("enter your name ","Rahul");
+// alert(v)
+
+// let dis=()=>{
+//     console.log("welcome to arrow function in js");
+// };
+// dis();
+
+// let sum = (x, y) => {
+//     return x+y
+// }
+// let res = sum(12,12)
+// console.log(res);
+
